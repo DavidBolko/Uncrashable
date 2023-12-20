c39375f2bcf7a03004033272552c916f30a45f9b
DavidBolko <davidbolko@outlook.com>
2023-12-20 15:20:29 +0100

    "small fix"

--- src/CollisionSystem.java
+++ src/CollisionSystem.java
@@ -3,6 +3,7 @@ public class CollisionSystem {
         if(player.getCar().getRoadLane() == car.getRoadLane()){
             if (car.getPosX() + 72 >= player.getCar().getPosX() && (car.getPosY() + 100 > player.getCar().getPosY() && car.getPosY() < player.getCar().getPosY()+100)){
                 player.getCar().explode();
+                car.explode();
                 SoundEngine.playExplosion();
                 game.gameOver();
             }
